110d25795f71b9f5bb146999393f8c6b8b98c566
Gukjin Choi <44183228+Blackcows@users.noreply.github.com>
2024-03-20 20:57:25 +0900

    Title: 외주 수익 최대화하기, Time: 102ms, Memory: 24MB, Status: Passed - CodeTree

--- 240320/외주 수익 최대화하기/max-of-outsourcing-profit.py
+++ 240320/외주 수익 최대화하기/max-of-outsourcing-profit.py
@@ -2,43 +2,57 @@ n = int(input())
 
 jobs = [list(map(int, input().split())) for _ in range (n)]
 
-dp = [0 for _ in range (n)]
+time = [job[0] for job in jobs] # 소요 시간
+benefit = [job[1] for job in jobs] # 보상
 
-index = -1
-for job in jobs:
-    index += 1 # index 이동
-    if index + job[0] > n:
-        # dp[index] = dp[index-1]
-        break # 날짜 넘어가면 break 처리
+dp = benefit[:]
 
-    time = [False for _ in range (n)]
+for i in range(n):
+    if time[i] + i > n:
+        dp[i] = 0
+    for j in range(i):
+        if time[j] <= i - j:
+            if time[i] + i <= n:
+                dp[i] = max(dp[i], dp[j] + benefit[i])
 
-    # == 이전 job과 충돌 여부 검증 ==
-    for i in range (index, index + job[0]):
-        # 현재 index를 true로
-        time[i] = True
+# print(dp)
+print(max(dp))
 
-    # merge가 되거나 처음에 도달하면 stop
-    mergeIndex = -1
+# ========= 이전 풀이 하던거 ==========
+# index = -1
+# for job in jobs:
+#     index += 1 # index 이동
+#     if index + job[0] > n:
+#         # dp[index] = dp[index-1]
+#         break # 날짜 넘어가면 break 처리
 
-    isConflict = False
-    for j in range (index-1, -1, -1):
-        for k in range (j, j + jobs[j][0]):
-            if time[k] == True:
-                isConflict = True
-                break
-            else:
-                isConflict = False
+#     time = [False for _ in range (n)]
 
-        if isConflict != True:
-            mergeIndex = j
-            break
-    # print("merge:", mergeIndex)
+#     # == 이전 job과 충돌 여부 검증 ==
+#     for i in range (index, index + job[0]):
+#         # 현재 index를 true로
+#         time[i] = True
 
-    for i in range (index, index + job[0]):
-        if isConflict == False:
-            dp[i] = max(dp[i-1], dp[i], job[1], dp[mergeIndex] + job[1])
-        else:
-            dp[i] = max(dp[i-1], dp[i], job[1])
-      
-print(dp[n-1])
+#     # merge가 되거나 처음에 도달하면 stop
+#     mergeIndex = -1
+
+#     isConflict = False
+#     for j in range (index-1, -1, -1):
+#         for k in range (j, j + jobs[j][0]):
+#             if time[k] == True:
+#                 isConflict = True
+#                 break
+#             else:
+#                 isConflict = False
+
+#         if isConflict != True:
+#             mergeIndex = j
+#             break
+#     # print("merge:", mergeIndex)
+
+#     for i in range (index, index + job[0]):
+#         if isConflict == False:
+#             dp[i] = max(dp[i-1], dp[i], job[1], dp[mergeIndex] + job[1])
+#         else:
+#             dp[i] = max(dp[i-1], dp[i], job[1])
+#     print(dp)
