14381ce7bd2d6ea7454414adb037f5560c8ca91e
Gukjin Choi <44183228+Blackcows@users.noreply.github.com>
2024-09-09 10:40:47 +0900

    Title: 최대 동전 거슬러주기, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240909/최대 동전 거슬러주기/max-coin-change.py
@@ -0,0 +1,17 @@
+n, m = map(int, input().split())
+coins = list(map(int, input().split()))
+coins.sort()
+
+dp = [0 for _ in range(m+1)]
+for coin in coins:
+    dp[coin] = 1
+
+for i in range(coins[0], m+1):
+    for coin in coins:
+        if i - coin < 0:
+            continue
+        
+        dp[i] = max(dp[i], dp[i-coin] + 1)
+
+ans = -1 if dp[m] == 0 else dp[m]
+print(ans)
